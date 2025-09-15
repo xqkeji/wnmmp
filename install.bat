a3d9557ee4f70ae7e0639c41385c3e223b449d83
xqkeji <xqkeji@foxmail.com>
2025-09-15 14:55:42 +0800

    Update install.bat
    修复环境变量冲突的问题

--- install.bat
+++ install.bat
@@ -14,21 +14,23 @@
 @echo off
 setlocal enabledelayedexpansion
 cd /d %~dp0
-REM find and stop service
-tasklist | findstr /i mongod.exe && taskkill /f /im mongod.exe
-tasklist | findstr /i mysqld.exe && taskkill /f /im mysqld.exe
-tasklist | findstr /i php-cgi.exe && taskkill /f /im php-cgi.exe
-tasklist | findstr /i nginx.exe && taskkill /f /im nginx.exe
 
 REM set variable
 set "script_dir_with_slash=%~dp0"
 set "HOME_DIR=%script_dir_with_slash:~0,-1%"
-set PATH=%PATH%;%HOME_DIR%\bin;%HOME_DIR%\mongodb\bin;%HOME_DIR%\mysql\bin;%HOME_DIR%\php;%HOME_DIR%\nginx
+set PATH=C:\Windows\System32;%HOME_DIR%\bin;%HOME_DIR%\mongodb\bin;%HOME_DIR%\mysql\bin;%HOME_DIR%\php;%HOME_DIR%\nginx;%PATH%
 set DATA_DIR=%HOME_DIR%\data
 set CONFIG_DIR=%HOME_DIR%\etc
 set LOGS_DIR=%HOME_DIR%\logs
 set TMP_DIR=%HOME_DIR%\tmp
 set DOWNLOAD_DIR=%TMP_DIR%\download
+
+
+REM find and stop service
+tasklist | findstr /i mongod.exe && taskkill /f /im mongod.exe
+tasklist | findstr /i mysqld.exe && taskkill /f /im mysqld.exe
+tasklist | findstr /i php-cgi.exe && taskkill /f /im php-cgi.exe
+tasklist | findstr /i nginx.exe && taskkill /f /im nginx.exe
 
 call "bin\install-vc-redist.bat"
 call "bin\download.bat"
