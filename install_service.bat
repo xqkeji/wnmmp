a99cc70e3d49bee7d7165fe91a51121b8bd05328
xqkeji <xqkeji@foxmail.com>
2025-10-26 08:31:13 +0800

    安装window服务时，默认先停止服务，防止有用户start.bat后，再去点击安装window服务

--- install_service.bat
+++ install_service.bat
@@ -18,12 +18,19 @@ setlocal enabledelayedexpansion
 set "script_dir_with_slash=%~dp0"
 set "HOME_DIR=%script_dir_with_slash:~0,-1%"
 
-set PATH=%PATH%;%HOME_DIR%\bin;%HOME_DIR%\mongodb\bin;%HOME_DIR%\mysql\bin;%HOME_DIR%\php;%HOME_DIR%\nginx
+set PATH=C:\Windows\System32;%HOME_DIR%\bin;%HOME_DIR%\mongodb\bin;%HOME_DIR%\mysql\bin;%HOME_DIR%\php;%HOME_DIR%\nginx;%PATH%
 set DATA_DIR=%HOME_DIR%\data
 set CONFIG_DIR=%HOME_DIR%\etc
 set LOGS_DIR=%HOME_DIR%\logs
 set TMP_DIR=!HOME_DIR!\tmp
 set INSTALL_FILE=%TMP_DIR%\install.lock
+
+REM find and stop service
+tasklist | findstr /i mongod.exe && taskkill /f /im mongod.exe
+tasklist | findstr /i mysqld.exe && taskkill /f /im mysqld.exe
+tasklist | findstr /i php-cgi.exe && taskkill /f /im php-cgi.exe
+tasklist | findstr /i nginx.exe && taskkill /f /im nginx.exe
+
 
 set "NSSM_PATH=%HOME_DIR%\bin\nssm.exe"
 
